50f427d57b2380882f39614710ad229b5d8e3860
Anastasiya <Tumanova_AD@mail.ru>
2019-12-11 22:05:20 +0300

    Added simple registration form

---
+++ client/src/components/RegForm/RegForm.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../../App.css';
+import './RegForm.css';
+
+function RegForm() {
+  return (
+    <div className="page">
+        <div className="form">
+            <div className="form-title">Регистрация</div>
+            <div className="form-page">
+                <div className="form-label">Фамилия:</div>
+                <input type="text"></input>
+                <div className="form-label">Имя:</div>
+                <input type="text"></input>
+                <div className="form-label">Отчество:</div>
+                <input type="text"></input>
+                <div className="form-label">Логин:</div>
+                <input type="text"></input>
+                <div className="form-label">Пароль:</div>
+                <input type="password"></input>
+                <div className="form-label">Повторите пароль:</div>
+                <input type="password"></input>
+            </div>
+            <div className="form-button">
+                Зарегистрироваться
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default RegForm;
