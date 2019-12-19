0e367539609d4d7fc10a17b2568bfa87fbc2f9dc
Horaigo <hiragen@yandex.ru>
2019-12-19 21:31:04 +0300

    Fixed login form a bit. Added some details to main form

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import RegComp from './components/RegForm/RegForm';
 import LoginComp from './components/LoginForm/LoginForm';
+import MainFormComp from './components/MainForm/mainForm';
 
 function App() {
   return (
-    LoginComp()
+    MainFormComp()
   );
 }
 
---
+++ client/src/components/MainForm/mainForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../../App.css';
+import './mainForm.css';
+
+function mainForm() {
+  return (
+    <div className="page">
+        <div className="form">
+            <div className="form-title">Главное меню</div>
+            <div className="form-page">
+                <div className="workzone">
+                    <div className="task-board">
+
+                    </div>
+                    <div className="sidemenu">
+                        <div className="button-container">
+                            
+                        </div>
+                        <div className="task-info">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default mainForm;
