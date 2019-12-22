716b7e0cd366a57d20c86d22f178cddc132ec654
Konstantin <cosstet@gmail.com>
2019-12-22 13:59:17 +0300

    Create separate client branch

--- server/database/db.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://test:test@timetracker-ncrqc.mongodb.net/test?retryWrites=true&w=majority")
-db = client.tracker
--- server/routes/test_route.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-test_route = Blueprint('test_route', __name__)
-@test_route.route('/')
-def hello_world():
-    return 'Hello World!'
